920d2db8f1fde02d8f1349bb04a8684a17e931fb
beardy-raccoon <77743494+beardy-raccoon@users.noreply.github.com>
2023-10-25 16:03:49 +0400

    refactor: redesign CandidatePreviewCard layout

--- src/components/CandidatePreviewCard/CandidatePreviewCard.tsx
+++ src/components/CandidatePreviewCard/CandidatePreviewCard.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Card, CardHeader, CardContent, CardActions, Box, Typography, IconButton, List, ListItem } from '@mui/material';
+import { Card, CardActions, Box, Typography, IconButton, List, ListItem } from '@mui/material';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
@@ -26,11 +26,27 @@ const CandidatePreviewCard: FC<ICandidatePreviewCard> = ({
 }) => {
   return (
     <Card className={styles.card}>
-      <Box>
-        <CardHeader title={name} subheader={position} sx={{ paddingBottom: 1 }} />
-        <CardContent sx={{ paddingTop: 0 }}>
-          <Typography variant="body2">{`Дата окончания курсов: ${graduated}`}</Typography>
-          <Typography variant="body2">{`${experience} года опыта`}</Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Box>
+          <Typography variant="body1">{name}</Typography>
+          <Typography variant="body1">{position}</Typography>
+        </Box>
+        <Typography variant="body2">Сегодня</Typography>
+        <Box display={'flex'} sx={isAvailable ? { color: '#5A9BFF' } : { color: '#B5B5B7' }}>
+          <FiberManualRecordIcon fontSize="small" sx={{ width: 15 }} />
+          <Typography variant="body2" sx={{ marginLeft: 1 }}>
+            В поиске
+          </Typography>
+        </Box>
+      </Box>
+
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', gap: 1 }}>
+            <Typography variant="body2">{`Опыт: ${experience} года`}</Typography>
+            <FiberManualRecordIcon fontSize="small" sx={{ width: 10 }} />
+            <Typography variant="body2">{`Дата окончания курсов: ${graduated}`}</Typography>
+          </Box>
           <List dense disablePadding sx={{ display: 'flex', gap: 1 }}>
             {stack?.map((item, index) => (
               <ListItem disablePadding key={index} sx={{ width: 'auto' }}>
@@ -41,16 +57,8 @@ const CandidatePreviewCard: FC<ICandidatePreviewCard> = ({
               </ListItem>
             ))}
           </List>
-        </CardContent>
-      </Box>
-      <Box className={styles.box}>
-        <Box display={'flex'} sx={isAvailable ? { color: '#5A9BFF' } : { color: '#B5B5B7' }}>
-          <FiberManualRecordIcon fontSize="small" sx={{ width: 15 }} />
-          <Typography variant="body2" sx={{ marginLeft: 1 }}>
-            В поиске
-          </Typography>
         </Box>
-        <CardActions disableSpacing sx={{ alignItems: 'flex-start', justifyContent: 'flex-end', padding: 0 }}>
+        <CardActions disableSpacing sx={{ alignItems: 'flex-end', padding: 0 }}>
           <IconButton aria-label="add to favorites" sx={{ padding: 0, color: '#1D6BF3' }}>
             {isFavorite ? <BookmarkIcon /> : <BookmarkBorderIcon />}
           </IconButton>
