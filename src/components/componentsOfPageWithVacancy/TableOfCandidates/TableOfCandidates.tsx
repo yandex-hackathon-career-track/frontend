cedc0ea56a79b15a0f0fb007a996465b577a2dfa
Art Frich <frich.g23@yandex.ru>
2023-10-29 05:19:20 +0500

    fix: handle err of TS

--- src/components/componentsOfPageWithVacancy/TableOfCandidates/TableOfCandidates.tsx
+++ src/components/componentsOfPageWithVacancy/TableOfCandidates/TableOfCandidates.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable react/prop-types */
-/* eslint-disable react/display-name */
 import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -9,7 +6,7 @@ import TableRow from '@mui/material/TableRow';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import Paper from '@mui/material/Paper';
-import { TableVirtuoso, TableComponents } from 'react-virtuoso';
+import { TableVirtuoso } from 'react-virtuoso';
 import RowTableContent from './RowTableContent';
 import HeaderOfTableOfCandidates from './HeaderOfTableOfCandidates';
 import { profiles } from '../../../utils/mockData';
@@ -19,13 +16,24 @@ export interface Data {
   resume?: boolean | string;
 }
 
-const VirtuosoTableComponents: TableComponents<Data> = {
+type VirtuosoTableComponentsType = {
+  Scroller: React.ForwardRefExoticComponent<React.RefAttributes<HTMLDivElement>>;
+  Table: React.FC;
+  TableHead: React.FC;
+  TableRow: React.FC;
+  TableBody: React.ForwardRefExoticComponent<React.RefAttributes<HTMLTableSectionElement>>;
+};
+
+const VirtuosoTableComponents: VirtuosoTableComponentsType = {
   Scroller: React.forwardRef<HTMLDivElement>((props, ref) => <TableContainer component={Paper} {...props} ref={ref} />),
   Table: (props) => <Table {...props} sx={{ borderCollapse: 'separate', tableLayout: 'fixed' }} />,
   TableHead,
-  TableRow: ({ item: _item, ...props }) => <TableRow {...props} />,
+  TableRow: (props) => <TableRow {...props} />,
   TableBody: React.forwardRef<HTMLTableSectionElement>((props, ref) => <TableBody {...props} ref={ref} />),
 };
+
+VirtuosoTableComponents.Scroller.displayName = 'VirtuosoTableScroller';
+VirtuosoTableComponents.TableBody.displayName = 'VirtuosoTableBody';
 
 export default function VirtTableOfCandidates() {
   return (
